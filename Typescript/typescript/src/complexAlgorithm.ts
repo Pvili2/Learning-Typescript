511fdbf840b178c622efb1194bcbbe7f263eded8
Padla Vili <pvilmos2003@gmail.com>
2024-04-22 20:45:00 +0200

    Refactor all algorithm to generic function

--- Typescript/typescript/src/complexAlgorithm.ts
+++ Typescript/typescript/src/complexAlgorithm.ts
@@ -1,20 +1,25 @@
 const numbers1: number[] = [1,2,3,4,5,7,11]; // ordered array / set
-const numbers2: number[] = [3,4,5,9];
+const numbers2: number[] = [3,4,5];
 const length1 = numbers1.length;
 const length2 = numbers2.length;
-function BinarySearch(numbers : number[], length: number, searchedValue: number):number{
+
+function BinarySearch<T>(numbers : T[], length: number, searchedValue: T, compare: Comparator<T>):number{
     let left = 0;
     let right = length - 1;
     let center = Math.round((left + right)/2);
 
-    while(left <= right && numbers[center] !== searchedValue){
-        if(numbers[center] > searchedValue){
+    let comparator = compare(numbers[center], searchedValue);
+
+    while(left <= right && comparator !== 0){
+        if(comparator > 0){
             right = center -1;
         }else{
             left = center + 1;
         }
 
         center = Math.round((left + right)/2);
+        //we need to update the comparator variable after we calculate the center
+        comparator = compare(numbers[center], searchedValue);
     }
     let idx = center;
     if(left <= right){
@@ -24,21 +29,22 @@ function BinarySearch(numbers : number[], length: number, searchedValue: number)
     }
 }
 
-console.log(BinarySearch(numbers1, length, 11));
+console.log(BinarySearch(numbers1, length1, 11, (a:number, b:number) => a-b));
 
-function Union(numbers1: number[], length1:number, numbers2: number[], length2: number):number[]{
-    let union:number[] = new Array(length1 + length2);
+function Union<T>(numbers1: T[], length1:number, numbers2: T[], length2: number, compare: Comparator<T>):T[]{
+    let union:T[] = new Array(length1 + length2);
     let i = 0;
     let j = 0;
     let db = -1;
 
     while(i< length1 && j < length2){
+        const comparator = compare(numbers1[i], numbers2[j]);
         db++;
-        if(numbers1[i] < numbers2[j]){
+        if(comparator < 0){
             union[db] = numbers1[i];
             i++;
         }else{
-            if(numbers1[i] > numbers2[j]){
+            if(comparator > 0){
                 union[db] = numbers2[j];
                 j++;
             }else{
@@ -62,8 +68,8 @@ function Union(numbers1: number[], length1:number, numbers2: number[], length2: 
     return union;
 }
 
-function deleteNullValues(numbers : number[]): number[] {
-    let newNumber: number[] = [];
+function deleteNullValues<T>(numbers : T[]): T[] {
+    let newNumber: T[] = [];
 
     numbers.forEach((item)=>{
 
@@ -73,35 +79,42 @@ function deleteNullValues(numbers : number[]): number[] {
     return newNumber;
 }
 
-console.log(deleteNullValues(Union(numbers1, length1, numbers2, length2)));
+console.log(deleteNullValues(Union(numbers1, length1, numbers2, length2, (a:number, b:number)=> a-b)));
 
-function arrayIsSubset(numbers1 : number[], length1: number, numbers2: number[], length2: number):boolean{
+function arrayIsSubset<T>(numbers1 : T[], length1: number, numbers2: T[], length2: number, compare: Comparator<T>):boolean{
     let i = 0;
     let j = 0;
-
-    while(i < length1 && j < length2 && numbers1[i] >= numbers2[j]){
-        if(numbers1[i] == numbers2[j]){
+    
+    let comparator = compare(numbers1[i], numbers2[j]);
+    while(i < length1 && j < length2 && comparator >= 0){
+        if(comparator === 0){
             i++;
         }
         j++;
+        comparator = compare(numbers1[i], numbers2[j]);
     }
 
     return i >= length1;
 }
 
 //numbers2 is a subset of numbers1? --- true/false
-console.log(arrayIsSubset(numbers2, length2, numbers1, length1));
+console.log(arrayIsSubset(numbers2, length2, numbers1, length1, (a: number, b: number)=>a-b));
 
-function arraysSection(numbers1: number[], length1: number, numbers2: number[], length2:number): number[]{
+interface Comparator<T>{
+    (a: T, b: T):number;
+}
+
+function arraysSection<T>(numbers1: T[], length1: number, numbers2: T[], length2:number, compare: Comparator<T>): T[]{
     let i = 0;
     let j = 0;
-    let section : number[] = new Array(length1 + length2);
+    let section : T[] = new Array(length1 + length2);
     let db = 0;
     while(i < length1 && j < length2){
-        if(numbers1[i] < numbers2[j]){
+        const comparison = compare(numbers1[i], numbers2[j])
+        if(comparison < 0){
             i++;
         }else{
-            if(numbers1[i] >numbers2[j]){
+            if(comparison > 0){
                 j++;
             }else{
                 section[db] = numbers1[i];
@@ -111,7 +124,9 @@ function arraysSection(numbers1: number[], length1: number, numbers2: number[], 
             }
         }
     }
-    return section;
+
+    //alternative way to return only the not null values
+    return section.filter((item) => item); 
 }
 
-console.log(deleteNullValues(arraysSection(numbers1, length1, numbers2, length2)));
+console.log(arraysSection<number>(numbers1, length1, numbers2, length2, (a:number,b:number)=> a-b));
